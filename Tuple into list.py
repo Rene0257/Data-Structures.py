3f621cfb6dfa20fdd8354d51beafe7e194071a23
Renesha Bhattacharjee <bhattacharjeerenesha32@gmail.com>
2025-10-27 05:12:45 +0530

    Add files via upload

---
+++ Tuple into list.py
@@ -0,0 +1,11 @@
+def convert(data):
+    list1 = []
+    for i in data:
+        list1.append(i)
+    print(list1)
+
+details = ("Renesha", "Bhattacharjee", "17", "5.4", "56", "Mathematics")
+print("Original Tuple:")
+print(details)
+print("Converted List:")
+convert(details)
